a8e8ba58a94c5be431c98eda14eb6e5424933176
francisleon <francisleon1186@yahoo.com>
2025-11-12 21:08:44 -0500

    factorial in py

--- factorial.py
+++ factorial.py
@@ -1,3 +1,13 @@
 def factorial(num):
-	# your code here
-	pass
+	
+	if num < 0:
+		return "Factorial is not defined for negative numbers."
+	elif num == 0 or num == 1:
+		return 1
+	else:
+		result=1
+		for i in range(2,num+1):
+			result = result *i
+		return result
+	
+#s	print(factorial(5))  # Example usage	
